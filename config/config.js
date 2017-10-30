5463779580142744e615c713aa9ff510f2f73d6b
HoneyHyunny imac <hyunseseo38@gmail.com>
2017-10-30 12:33:32 +0900

    1030
    ///

--- config/config.js
+++ config/config.js
@@ -6,7 +6,8 @@
 
 module.exports = {
 	server_port: 3000,
-	db_url: 'mongodb://admin20000:3590dany@ds155414.mlab.com:55414/heroku_53dpsf7n',
+	// db_url: 'mongodb://admin20000:3590dany@ds155414.mlab.com:55414/heroku_53dpsf7n',
+	db_url:'mongodb://localhost:27017/local',
 	db_schemas: [
     {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
     ,{file:'./post_schema', collection:'post', schemaName:'PostSchema', modelName:'PostModel'}
